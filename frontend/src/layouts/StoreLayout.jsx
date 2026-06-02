73d772195c8421b3c4ac42363b9f78305f64102d
Sakuna Thejan <sakuthejan200515@gmail.com>
2026-06-02 08:57:28 +0530

    feat: create StoreLayout component with integrated navigation, announcements, and page transitions

--- frontend/src/layouts/StoreLayout.jsx
+++ frontend/src/layouts/StoreLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { AnnouncementProvider } from "../context/AnnouncementContext.jsx";
 import AnnouncementManager from "../components/announcements/AnnouncementManager.jsx";
 import BottomNav from "../components/layout/BottomNav.jsx";
@@ -6,20 +6,25 @@ import Footer from "../components/layout/Footer.jsx";
 import Navbar from "../components/layout/Navbar.jsx";
 import PageTransition from "../components/transitions/PageTransition.jsx";
 
-const StoreLayout = () => (
-  <AnnouncementProvider>
-    <div className="min-h-screen bg-[#f7f7f4] dark:bg-graphite">
-      <Navbar />
-      <AnnouncementManager />
-      <main className="has-bottom-nav">
-        <PageTransition>
-          <Outlet />
-        </PageTransition>
-      </main>
-      <BottomNav />
-      <Footer />
-    </div>
-  </AnnouncementProvider>
-);
+const StoreLayout = () => {
+  const { pathname } = useLocation();
+  const hideAnnouncements = pathname === "/login";
+
+  return (
+    <AnnouncementProvider>
+      <div className="min-h-screen bg-[#f7f7f4] dark:bg-graphite">
+        <Navbar />
+        {!hideAnnouncements && <AnnouncementManager />}
+        <main className="has-bottom-nav">
+          <PageTransition>
+            <Outlet />
+          </PageTransition>
+        </main>
+        <BottomNav />
+        <Footer />
+      </div>
+    </AnnouncementProvider>
+  );
+};
 
 export default StoreLayout;
